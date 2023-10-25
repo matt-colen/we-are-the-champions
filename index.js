c749855c798ba55720a98d52e5a684778220de91
matt-colen <mzcolen@gmail.com>
2023-10-24 21:31:10 -0500

    Refactors JS & CSS

--- index.js
+++ index.js
@@ -7,24 +7,25 @@ import {
   onValue,
 } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-database.js";
 
-// Firebase Realtime DB setup
+// Firebase app config & setup
 const appSettings = {
   databaseURL:
     "https://we-are-the-champions-20eb9-default-rtdb.firebaseio.com/",
 };
+const app = initializeApp(appSettings); // Creates Firebase app
+const database = getDatabase(app); // Creates database obj
+const endorsementListInDB = ref(database, "endorsements"); // Creates reference to database location
 
-const app = initializeApp(appSettings);
-const database = getDatabase(app);
-const endoresementsInDB = ref(database, "endorsements");
-
+// DOM elements
 const endorsementInput = document.querySelector("#user-input__endorsement");
 const fromInput = document.querySelector("#message-details__from");
 const toInput = document.querySelector("#message-details__to");
 const endorsementList = document.querySelector("#endorsement__list");
 const publishBtn = document.querySelector("#publish-btn");
 
-// Function that pushes the endorsement object to the DB
+// Function that captures input values & pushes to the DB
 const addEndorsementToDB = () => {
+  // Captures all of the current DOM element values
   const endorsementInputValue = endorsementInput.value;
   const fromInputValue = fromInput.value;
   const toInputValue = toInput.value;
@@ -35,25 +36,39 @@ const addEndorsementToDB = () => {
     to: toInputValue,
   };
 
-  push(endoresementsInDB, endorsement);
+  // Pushes the endorsement obj to the DB
+  push(endorsementListInDB, endorsement);
 };
 
-// Function that runs the callback on changes to the DB reference
-onValue(endoresementsInDB, (snapshot) => {
+// Event listener for 'Enter' key on "To" input field (keyboard users)
+toInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    addEndorsementToDB();
+  }
+});
+
+// Event listener for "Publish" btn click
+publishBtn.addEventListener("click", () => {
+  addEndorsementToDB();
+});
+
+// Firebase method that runs the callback function on each update to the DB reference
+onValue(endorsementListInDB, (snapshot) => {
   if (snapshot.exists()) {
     const snapshotValue = snapshot.val();
-    const snapshotValuesArray = Object.values(snapshotValue).reverse();
-    const reversedSnapshotArray = snapshotValuesArray.reverse();
+    const snapshotValuesArray = Object.values(snapshotValue);
+    const reversedSnapshotArray = snapshotValuesArray.reverse(); // Ensures that the newest endorsement shows first
 
     renderCards(reversedSnapshotArray);
   } else {
-    endorsementList.textContent = "No endorsements yet";
+    endorsementList.textContent =
+      "No endorsements yet, add a message for your team 🥳";
   }
 });
 
 // Function that renders the endorsement cards
 const renderCards = (array) => {
-  clearList();
+  clearList(); // Clears existing list
 
   const cards = array.map((obj) => {
     return createCard(obj);
@@ -61,7 +76,7 @@ const renderCards = (array) => {
 
   cards.forEach((card) => endorsementList.append(card));
 
-  clearInputs();
+  clearInputs(); // Clear input field values
 };
 
 // Function that creates the endorsement cards
@@ -95,13 +110,3 @@ const clearInputs = () => {
 
   inputs.forEach((input) => (input.value = ""));
 };
-
-toInput.addEventListener("keypress", (e) => {
-  if (e.key === "Enter") {
-    addEndorsementToDB();
-  }
-});
-
-publishBtn.addEventListener("click", () => {
-  addEndorsementToDB();
-});
